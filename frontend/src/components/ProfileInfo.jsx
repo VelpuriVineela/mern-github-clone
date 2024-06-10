2711492bfd5a1fb4c26b8f09c48425d5b9bca0bf
velpuri vineela <vineelavelpuri@gmail.com>
2024-06-10 17:01:41 +0530

    github api integrated

--- frontend/src/components/ProfileInfo.jsx
+++ frontend/src/components/ProfileInfo.jsx
@@ -7,25 +7,30 @@ import {
 import { FaXTwitter } from "react-icons/fa6";
 import { TfiThought } from "react-icons/tfi";
 import { FaEye } from "react-icons/fa";
+import { formatMemberSince } from "../utils/functions";
+// import LikeProfile from "./LikeProfile";
 
-const ProfileInfo = () => {
-  const userProfile = {
-    avatar_url: "https://octodex.github.com/images/femalecodertocat.png",
-    bio: "👨🏻‍💻👨🏻‍💻👨🏻‍💻",
-    email: "vineelavel@gmail.com",
-    followers: 100,
-    following: 200,
-    html_url: "https://github.com/VelpuriVineela",
-    location: "Somewhere, Earth",
-    name: "vineela velpuri",
-    public_gists: 100,
-    public_repos: 100,
-    twitter_username: "vineelavel",
-    login: "vineelavel",
-  };
+const ProfileInfo = ({ userProfile }) => {
+  // const userProfile = {
+  // 	avatar_url: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745",
+  // 	bio: "👨🏻‍💻👨🏻‍💻👨🏻‍💻",
+  // 	email: "johndoe@gmail.com",
+  // 	followers: 100,
+  // 	following: 200,
+  // 	html_url: "https://github.com/burakorkmez",
+  // 	location: "Somewhere, Earth",
+  // 	name: "John Doe",
+  // 	public_gists: 100,
+  // 	public_repos: 100,
+  // 	twitter_username: "johndoe",
+  // 	login: "johndoe",
+  // };
+
+  //fot the time of created profile
+  const memberSince = formatMemberSince(userProfile?.created_at);
 
   return (
-    <div className="lg:w-1/3 w-full flex flex-col gap-2 md:sticky md:top-10">
+    <div className="lg:w-1/3 w-full flex flex-col gap-2 lg:sticky md:top-10">
       <div className="bg-glass rounded-lg p-4">
         <div className="flex gap-3 items-center">
           {/* User Avatar */}
@@ -37,9 +42,11 @@ const ProfileInfo = () => {
             />
           </a>
           {/* View on Github */}
+
           <div className="flex gap-2 items-center flex-col">
+            {/* <LikeProfile userProfile={userProfile} /> */}
             <a
-              href={userProfile.html_url}
+              href={userProfile?.html_url}
               target="_blank"
               rel="noreferrer"
               className="bg-glass font-medium w-full text-xs p-2 rounded-md cursor-pointer border border-blue-400 flex items-center gap-2"
@@ -51,7 +58,6 @@ const ProfileInfo = () => {
         </div>
 
         {/* User Bio */}
-        {/* if the user has bio it will show ortherwise it shoes nothing */}
         {userProfile?.bio ? (
           <div className="flex items-center gap-2">
             <TfiThought />
@@ -83,7 +89,7 @@ const ProfileInfo = () => {
         {/* Member Since Date */}
         <div className="my-2">
           <p className="text-gray-600 font-bold text-sm">Member since</p>
-          <p className="">21 Sep, 2023</p>
+          <p className="">{memberSince}</p>
         </div>
 
         {/* Email Address */}
@@ -137,5 +143,4 @@ const ProfileInfo = () => {
     </div>
   );
 };
-
 export default ProfileInfo;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,3 +1,5 @@
+import { useCallback, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import ProfileInfo from "../components/ProfileInfo";
 import Repos from "../components/Repos";
 import Search from "../components/Search";
@@ -5,14 +7,89 @@ import SortRepos from "../components/SortRepos";
 import Spinner from "../components/Spinner";
 
 const HomePage = () => {
+  // to fetch data of the userprofile
+  const [userProfile, setUserProfile] = useState(null);
+  // to fetch the data of repos of that user
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  //for the sorting functionality accroding to most recent , most stars and most forks
+  const [sortType, setSortType] = useState("recent");
+
+  // handle the getUserProfileAndRepos function
+  // uses the callback function to get not be in of infinite loop
+  const getUserProfileAndRepos = useCallback(
+    async (username = "VelpuriVineela") => {
+      setLoading(true);
+      try {
+        // fetch the user Profile
+        const userRes = await fetch(`https://api.github.com/users/${username}`);
+        const userProfile = await userRes.json();
+        setUserProfile(userProfile);
+
+        //fetch the respos of the user
+        const repoRes = await fetch(userProfile.repos_url);
+        const repos = await repoRes.json();
+        repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)); //descending, recent first
+        setRepos(repos);
+
+        return { userProfile, repos };
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    },
+    []
+  );
+
+  // to fetch the data
+  useEffect(() => {
+    getUserProfileAndRepos();
+  }, [getUserProfileAndRepos]);
+
+  // handle the searching functionality by the username
+  const onSearch = async (e, username) => {
+    e.preventDefault();
+
+    // setting to the intial values (empty values)
+    setLoading(true);
+    setRepos([]);
+    setUserProfile(null);
+
+    // get the search usered detailes for the getuserprofileandrepo
+    const { userProfile, repos } = await getUserProfileAndRepos(username);
+
+    //updatting the userProfile values and the repos
+    setUserProfile(userProfile);
+    setRepos(repos);
+    setLoading(false);
+    setSortType("recent");
+  };
+
+  const onSort = (sortType) => {
+    if (sortType === "recent") {
+      repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)); // descending, recent first
+    } else if (sortType === "stars") {
+      repos.sort((a, b) => b.stargazers_count - a.stargazers_count); // descending, most stars first
+    } else if (sortType === "forks") {
+      repos.sort((a, b) => b.forks_count - a.forks_count); //descending, ,most forks first
+    }
+
+    //updating the order of repos(sort)
+    setSortType(sortType);
+    // updating repos
+    setRepos([...repos]);
+  };
+
   return (
     <div className="m-4">
-      <Search />
-      <SortRepos />
+      <Search onSearch={onSearch} />
+      {repos.length > 0 && <SortRepos onSort={onSort} sortType={sortType} />}
       <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
-        <ProfileInfo />
-        <Repos />
-        <Spinner />
+        {userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
+        {!loading && <Repos repos={repos} />}
+        {loading && <Spinner />}
       </div>
     </div>
   );
